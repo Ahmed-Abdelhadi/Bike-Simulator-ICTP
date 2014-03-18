dd52708a29d703140b7f72508d9c9fc0baf3afe6
krprathyu <prathyushakr@gmail.com>
2014-03-18 15:55:47 +0100

    Added animate and terrain

--- terrain.py
+++ terrain.py
@@ -1,1 +1,15 @@
 # Terrain
+import numpy as np
+import scipy as sc
+import matplotlib.pyplot
+import random as rn
+tx=[]
+tm=[]
+ty=[]
+for i in xrange(1,10):
+ tx.append(i)
+ tm.append(-0.5)
+ ty.append(-1.0)
+
+
+ 
