dda0cee08c805b6fa0fa5044e89535929250af26
Ahmed-Abdelhadi <hady.bio@gmail.com>
2014-03-18 18:56:30 +0100

    Change_the_Declaratio_Method_of_the_Array

--- Bike_start.py
+++ Bike_start.py
@@ -10,17 +10,17 @@ class Bike(object):
     #self.x=[x1,x2,x3,x4]
     #self.y=[y1,y2,y3,y4]
     self.pos=[[4., 4.], [9., 4.], [9., 9.], [4., 9.]]
-    self.angle=[0.]*4
+    self.angle=[0. for i in xrange (4)]
     self.r=[1., 1., 0., 0.]
     self.m=[1., 1., 1., 1.]
     #self.vx=[vx1,vx2,vx3,vx4]
     #self.vy=[vy1,vy2,vy3,vy4]
-    self.v=[[0., 0.]]*4
-    self.w=[0.]*4
-    self.a=[[0., 0.]]*4
-    self.ang_a=[0.]*4#[ang_a1,ang_a2,ang_a3,ang_a4]
-    self.k=[[0.]*4]*4
-    self.l=[[0.]*4]*4
+    self.v=[[0. for i in xrange (2)] for i in xrange(4)]
+    self.w=[0. for i in xrange (4)]
+    self.a=[[0. for i in xrange (2)] for i in xrange(4)]
+    self.ang_a=[0. for i in xrange (4)]#[ang_a1,ang_a2,ang_a3,ang_a4]
+    self.k=[[0. for i in xrange (4)] for i in xrange(4)]
+    self.l=[[0. for i in xrange (4)] for i in xrange(4)]
     
     #self.SpringDist=[self.CalcDist(x1,y1,x2,y2),self.CalcDist(x1,y1,x3,y3),self.CalcDist(x1,y1,x4,y4),self.CalcDist(x2,y2,x3,y3),self.CalcDist(x2,y2,x4,y4),self.CalcDist(x3,y3,x4,y4)]
     #self.k=[1,1,1,1,1,1]
