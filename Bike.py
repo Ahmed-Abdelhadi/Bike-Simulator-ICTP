8e21eed69d4b83706412376db610c3056676a54e
Ahmed-Abdelhadi <hady.bio@gmail.com>
2014-03-18 23:27:58 +0100

    Produce Random Bike

--- Bike.py
+++ Bike.py
@@ -1,63 +1,112 @@
 import math
+import random
 
 class Bike(object):
   #def CalcDist(self,x1,y1,x2,y2):
    # distance=((x2-x1)**2+(y2-y1)**2)**0.5
    # return distance
 
-  def __init__(self,x1,x2,x3,x4,y1,y2,y3,y4,r1,r2,r3,r4,m1,m2,m3,m4,vx1,vx2,vx3,vx4,vy1,vy2,vy3,vy4,ang_v1,ang_v2,ang_v3,ang_v4,a1,a2,a3,a4,ang_a1,ang_a2,ang_a3,ang_a4,k1,k2,k3,k4,k5,k6):
+  def __init__(self):
     #self.x=[x1,x2,x3,x4]
     #self.y=[y1,y2,y3,y4]
-    self.P=[[x1,y1],[x2,y2],[x3,y3],[x4,y4]]
-    self.r=[r1,r2,r3,r4]
-    self.m=[m1,m2,m3,m4]
+    self.pos=[[4., 4.], [9., 4.], [9., 9.], [4., 9.]]
+    self.angle=[0. for i in xrange (4)]
+    self.r=[1., 1., 0., 0.]
+    self.m=[1., 1., 1., 1.]
     #self.vx=[vx1,vx2,vx3,vx4]
     #self.vy=[vy1,vy2,vy3,vy4]
-    self.v=[[vx1,vy1],[vx2,vy2],[vx3,vy3],[vx4,vy4]]
-    self.ang_v=[ang_v1,ang_v2,ang_v3,ang_v4]
-    self.a=[a1,a2,a3,a4]
-    self.ang_a=[ang_a1,ang_a2,ang_a3,ang_a4]
-    self.SpringDist=[self.CalcDist(x1,y1,x2,y2),self.CalcDist(x1,y1,x3,y3),self.CalcDist(x1,y1,x4,y4),self.CalcDist(x2,y2,x3,y3),self.CalcDist(x2,y2,x4,y4),self.CalcDist(x3,y3,x4,y4)]
-    self.k=[k1,k2,k3,k4,k5,k6]
-  
-  def CalcDist(self,x1,y1,x2,y2): 
-    distance=((x2-x1)**2+(y2-y1)**2)**0.5
-    return distance
-  
-  def cc
-  
+    self.v=[[0. for i in xrange (2)] for i in xrange(4)]
+    self.w=[0. for i in xrange (4)]
+    self.a=[[0. for i in xrange (2)] for i in xrange(4)]
+    self.ang_a=[0. for i in xrange (4)]#[ang_a1,ang_a2,ang_a3,ang_a4]
+    self.k=[[0. for i in xrange (4)] for i in xrange(4)]
+    self.l=[[0. for i in xrange (4)] for i in xrange(4)]
+    
+    #self.SpringDist=[self.CalcDist(x1,y1,x2,y2),self.CalcDist(x1,y1,x3,y3),self.CalcDist(x1,y1,x4,y4),self.CalcDist(x2,y2,x3,y3),self.CalcDist(x2,y2,x4,y4),self.CalcDist(x3,y3,x4,y4)]
+    #self.k=[1,1,1,1,1,1]
+    for i in xrange (4):
+      for j in xrange(4):
+	self.k[i][j]=1
+	dx=self.pos[j][0]-self.pos[i][0]
+	dy=self.pos[j][1]-self.pos[i][1]
+	dr = math.sqrt(dx*dx + dy*dy)
+	self.l[i][j]=dr
+    
   def GenerateBike():
-    GeneratePts()
+    #GeneratePts()
     GenerateRadius()
     GenerateMass()
     GenerateSpring()    
 
-  def GeneratePts():
+  def GeneratePts(self, object,r, MaxWidth, MaxHeigth):
+    for i in xrange(4):
+      if i==0:
+	xCoordinateL=r
+	xCoordinateU=MaxWidth-r
+	yCoordinateL=r
+	yCoordinateU=MaxHeigth-r
+	object.pos[i]=[random.randint(xCoordinateL, xCoordinateU), random.randint(yCoordinateL, yCoordinateU)]
+      if i==1:
+	xCoordinateL=r
+	xCoordinateU=MaxWidth-r
+	yCoordinateL=r
+	yCoordinateU=MaxHeigth-r
+	while True:
+	  object.pos[i]=[random.randint(xCoordinateL, xCoordinateU), random.randint(yCoordinateL, yCoordinateU)]
+	  D1=math.sqrt((object.pos[i][0]-object.pos[0][0])**2 + (object.pos[i][1]-object.pos[0][1])**2)
+	  if D1>2*r:
+	    break;
+
+      if i==2:
+	pass
+      
+      if i==3:
+	pass
+    
+
+  def GenerateRadius(self,object):
+    object.r=[3,3,0,0]
+    #pass
+
+  def GenerateMass(self,object):
+    object.m=[10,10,10,10]
+    #pass
+
+  def GenerateSpring(self,object):
+    for i in xrange (4):
+      for j in xrange(4):
+	object.k[i][j]=1
+	dx=object.pos[j][0]-object.pos[i][0]
+	dy=object.pos[j][1]-object.pos[i][1]
+	dr = math.sqrt(dx*dx + dy*dy)
+	object.l[i][j]=dr
+  
+  def RandomBike():
     pass
-
-  def GenerateRadius():
+  
+  def UniformBike():
     pass
+  
+def column(matrix, i):
+    return [row[i] for row in matrix]
+H=Bike()    
+#print random.randint(4, 10)
+#print column(H.pos,1)
+print H.pos
+H.GeneratePts(H,4,30,30)
+print H.pos
 
-  def GenerateMass():
-    pass
+print H.m
+H.GenerateMass(H)
+print H.m
 
-  def GenerateSpring():
-    pass
+print H.r
+H.GenerateRadius(H)
+print H.r
 
- 
-Bike1= Bike(4,8,9,3,4,5,5,8,1,3,5,6,3,4,1,8,9,3,4,5,3,5,6,3,4,8,5,1,2,4,2,8,9,3,4,7,5,3,5,6,3,4)
-
-
-print "Points are=",Bike1.P
-print "r=",Bike1.r
-print"m=",Bike1.m
-print"v=",Bike1.v
-print"ang_v=",Bike1.ang_v
-print"a=",Bike1.a
-print"ang_a=",Bike1.ang_a
-print "SpringDist=",Bike1.SpringDist
-print "K=",Bike1.k
-#D12=math.sqrt(4)
-D12=Bike1.CalcDist(9,12,3,4)
-print D12
+print H.l
+print H.k
+H.GenerateSpring(H)
+print H.l
+print H.k
 
