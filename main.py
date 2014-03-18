d5825c2bc7bcf5074edcfe78fed1377b4a3976db
avoronts <avoronts@hp83-inf-33.ictp.it>
2014-03-18 20:32:04 +0100

    main function and bake_factory class

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 # main program for group 4
+from terrain import Terrain
+from bike_factory import Bike_factory
+from visual import Visual
 
-terrain = Terrain_class()
+terrain = Terrain()
 
+for i in xrange(1):
+    factory = Bike_factory(20)
 
 #go simulation
-new_bike = Bike_class()
+    for new_bike in factory():
+    
+        physics = Physics_class(new_bike,terrain)
+        visual  = Visual(new_bike,terrain)
+
+        while physics.make_step(): 
+	    visual.plot()		# some visual
+
+# get some data from physics and story the bike result
+        new_bike.set_result(physics.get_result())
 
 
-physics = Physics_class(new_bike,terrain)
+    factory.make_new_generation()		#make some improovements genetics
 
-while physics.make_step(): 
-#    some visual
-    pass
-
-# get some data from physics
-
-
-#story the bike and the result
-
-#make some improovements genetics
-
-#do it  one more
