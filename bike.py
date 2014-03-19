b4810d1083ef68fdb0cda720108aa9bb70639634
Ahmed-Abdelhadi <hady.bio@gmail.com>
2014-03-19 16:48:55 +0100

    Chang_variable_names_and_class_name

--- bike.py
+++ bike.py
@@ -7,7 +7,7 @@ class bike_class(object):
     self.position=[[4., 4.], [9., 4.], [9., 9.], [4., 9.]]
     self.angle=[0. for i in xrange (4)]
     self.radius=[1., 1., 0., 0.]
-    self.mass=[1., 1., 1., 1.]
+    self.mass=[10., 10., 1., 1.]
     self.velocity=[[0. for i in xrange (2)] for i in xrange(4)]
     self.angular_velocity=[0. for i in xrange (4)]
     self.acceleration=[[0. for i in xrange (2)] for i in xrange(4)]
@@ -17,7 +17,7 @@ class bike_class(object):
     
     for i in xrange (4):
       for j in xrange(4):
-	self.spring_constant[i][j]=1
+	self.spring_constant[i][j]=500
 	dx=self.position[j][0]-self.position[i][0]
 	dy=self.position[j][1]-self.position[i][1]
 	dr = math.sqrt(dx*dx + dy*dy)
